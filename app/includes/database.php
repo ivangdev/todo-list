cfc7bc869279b561078ff48c7a71c03edf387dd1
Ivan Guerrero <ivangvprogrammer@gmail.com>
2025-05-31 21:06:08 -0500

    feat: Establecer conexión a la base de datos con PDO
    Introduce el script `app/includes/database.php` que gestiona la conexión a la base de datos utilizando PDO.
    
    Este script se encarga de:
    - Cargar las credenciales de la base de datos (host, nombre, usuario, contraseña, charset) de forma segura desde variables de entorno.
    - Implementar un manejo básico de errores para fallos durante el intento de conexión.

---
+++ app/includes/database.php
@@ -0,0 +1,22 @@
+<?php
+
+// Carga las variables de entorno desde el archivo .env
+$host = $_ENV['DB_HOST'] ?? '';
+$db = $_ENV['DB_NAME'] ?? '';
+$user = $_ENV['DB_USER'] ?? '';
+$pass = $_ENV['DB_PASS'] ?? '';
+
+// Charset de la base de datos
+$charset = $_ENV['DB_CHARSET'] ?? 'utf8mb4';
+
+// Data Source Name (DSN) para la conexión a la base de datos
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+// Intenta establecer la conexión a la base de datos utilizando PDO
+try {
+  $pdo = new PDO($dsn, $user, $pass);
+  echo "Conexión exitosa a la base de datos $db en $host";
+} catch (PDOException $event) {
+  echo "Error al conectar a la base de datos: " . $event->getMessage();
+  exit; // Termina la ejecución si hay un error de conexión
+}
